d11434e6e73aaff544f5cc9096b50b6cced79fc6
fernandozhs <fernandozhs@gmail.com>
2021-12-25 22:33:02 -0500

    Code clean-up.

--- src/elements/elements.cpp
+++
@@ -1,8 +0,0 @@
-/*
-  Due to the recursive relationship that exists between some of the methods featuring in the `Link` and `Node` classes, their files are collected here in order to facilitate their joint compilation.
-*/
-
-#include "../elements/elements.hpp"
-
-#include "../elements/link.cpp"
-#include "../elements/node.cpp"
--- src/elements/elements.hpp
+++
@@ -1,8 +0,0 @@
-/*
-  Due to the recursive relationship that exists between some of the methods featuring in the `Link` and `Node` classes, their header files are collected here in order to facilitate their joint compilation.
-*/
-
-#pragma once
-
-#include "link.hpp"
-#include "node.hpp"
